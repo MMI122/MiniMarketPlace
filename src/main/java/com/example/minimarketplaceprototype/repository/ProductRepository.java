38e2386c7af02273fdac1a02493b2c35489212c1
Mehedi Hasan Rabby <rabby2107086@stud.kuet.ac.bd>
2026-03-24 01:55:58 +0600

    add ProductController, ProductService, and Thymeleaf views

--- src/main/java/com/example/minimarketplaceprototype/repository/ProductRepository.java
+++ src/main/java/com/example/minimarketplaceprototype/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.minimarketplaceprototype.repository;
 
-public interface ProductRepository {
+import com.example.minimarketplaceprototype.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findBySellerId(Long sellerId);
 }
